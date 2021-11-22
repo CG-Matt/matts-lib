c9d89502a7a4d8e2d76fc2cc6f321129d28af680
CG-Matt <38221030+CG-Matt@users.noreply.github.com>
2021-11-22 22:18:35 +0000

    New function added
    Added new function (matts_generation_ore), and updated readme.md with instrucitons on usage

--- functions.lua
+++ functions.lua
@@ -1,3 +1,99 @@
+local resource_autoplace = require("resource-autoplace")
+
+function matts_generation_ore(mod_properties, ore_properties, autoplace_properties)
+	-- Defines mod_name if not defined
+	if mod_properties.mod_name == nil then mod_properties.mod_name = "" end
+
+	-- Sets default graphic path
+	if mod_properties.icon_path == nil then mod_properties.icon_path = "graphics/icons" end
+	if mod_properties.ore_path == nil then mod_properties.ore_path = "graphics/entity" end
+
+	-- Attaches prefix to internal name if one is supplied
+	if not mod_properties.prefix == nil then
+		ore_properties.name = mod_properties.prefix .. "-" .. ore_properties.name
+	end
+
+	-- Sets default mining time
+	if ore_properties.mining_time == nil then ore_properties.mining_time = 1 end
+
+	-- Sets mining result to ore name by default, result is only intended to be used when the result is different from the ore name
+	if ore_properties.result == nil then ore_properties.result = ore_properties.name end
+
+	-- Sets the default mining fluid amount to 10 if not provided
+	if not ore_properties.mining_fluid == nil and ore_properties.mining_fluid_amount == nil then ore_properties.mining_fluid_amount = 10 end
+
+	-- Sets default multipliers
+	if autoplace_properties.regular_multiplier == nil then autoplace_properties.regular_multiplier = 1 end
+	if autoplace_properties.starting_area_multiplier == nil then autoplace_properties.starting_area_multiplier = 1.1 end
+
+	-- Sets which ore path system it whould use
+	if mod_properties.custom_paths == true then
+		icon_path = mod_properties.custom_icon_path
+		ore_path = mod_properties.custom_ore_path
+		hr_ore_path = mod_properties.custom_hr_ore_path
+	else
+		icon_path = "__" .. mod_properties.mod_name .. "__/" .. mod_properties.icon_path .. "/" .. ore_properties.name .. ".png"
+		ore_path = "__" .. mod_properties.mod_name .. "__/" .. mod_properties.ore_path .. "/" .. ore_properties.name .. ".png"
+		hr_ore_path = "__" .. mod_properties.mod_name .. "__/" .. mod_properties.ore_path .. "/hr-" .. ore_properties.name .. ".png"
+	end
+
+	return
+	{
+		type = "resource",
+		name = ore_properties.name,
+		icon = icon_path,
+		icon_size = 64, icon_mipmaps = 4,
+		flags = {"placeable-neutral"},
+		order = ore_properties.order,
+		tree_removal_probability = 0.8,
+		tree_removal_max_distance = 32 * 32,
+		minable =
+		{
+			-- mining_particle = 
+			mining_time = ore_properties.mining_time,
+			result = ore_properties.result,
+			mining_fluid = ore_properties.fluid,
+			mining_fluid_amount = ore_properties.fluid_amount
+		},
+		-- walking_sound = 
+		collision_box = {{-0.1, -0.1},{0.1, 0.1}},
+		selection_box = {{-0.5, -0.5},{0.5, 0.5}},
+		autoplace = resource_autoplace.resource_autoplace_settings
+		{
+			name = ore_properties.name,
+			order = ore_properties.order,
+			base_density = autoplace_properties.base_density,
+			has_starting_area_placement = autoplace_properties.in_starting_area,
+			regular_rq_factor_multiplier = autoplace_properties.regular_multiplier,
+			starting_rq_factor_multiplier = autoplace_properties.starting_area_multiplier
+		},
+		stage_counts = {15000, 9500, 5500, 2900, 1300, 400, 150, 80},
+		stages =
+		{
+			sheet =
+			{
+				filename = ore_path,
+				priority = "extra-high",
+				width = 64,
+				height = 64,
+				frame_count = 8,
+				variation_count = 8,
+				hr_version =
+				{
+					filename = hr_ore_path,
+					priority = "extra-high",
+					size = 128,
+					frame_count = 8,
+					variation_count = 8,
+					scale = 0.5
+				}
+			}
+		},
+		map_color = ore_properties.map_color
+	}
+
+end
+
 function matts_oregen_apc(ore, order)
 	return
 	{
--- readme.md
+++ readme.md
@@ -12,6 +12,9 @@ If you are interested in using my mods functions for ore generation please read 
 * matts_oregen_nl -> Noise Layer
 * matts_oregen_ore -> Ore entity
 * matts_oregen_item -> Ore item
+* matts_generation_ore -> Ore entity (New)
+
+### The following only applies when using the old functions
 
 Translations:
 
@@ -31,3 +34,33 @@ Icons/Sprite sheets:
   2. Ore SPRITESHEETS must be located under 'your mod name'/graphics/entity/ores/'ore name', and must be named "ore.png" and "hr-ore.png"
 * Please also note that all functions are INDEPENDENT of each other and can be used separately if only some functions are required.
 * For any further questions please start a discussion on the mod page.
+
+### The following only applies when using the new functions
+
+Function format -> matts_generation_ore(mod_properties(as dictionary), ore_properties(as dictionary), autoplace_properties(as dictionary))
+The new format follows a more vanilla-ish approach to ore generation
+
+* Valid values for mod_properties:
+  * mod_name -> Name of your mod e.g. matts-lib (not required if using custom graphic paths)
+  * icon_path ->Specifies the folder in which your ore icon graphic is located (not required if using default path: "graphics/icons")
+  * ore_path -> Specifies the folder in which your ore entity graphics are located (not required if using default path: "graphics/entity")
+  * prefix -> Specifies a prefix for the ore internal names, can be useful if creating an ore that is common among other mods but want to avoid conflict(optional)
+  * custom_paths -> Set to true if you wish to use custom graphic paths that dont follow the structure specified in this mod(optional)
+  * custom_icon_path -> Specifies the custom path for the ore icon(required if using custom_paths)
+  * custom_ore_path -> Specifies the custom path for the ore entity(required if using custom_paths)
+  * custom_hr_ore_path -> Specifies the custom path for the high resolution ore entity(required if using custom_paths)
+
+* Valid values for ore_properties:
+  * name -> The name of the ore
+  * order -> Specifies the order in which your ore should be placed
+  * map_color -> Specifies the map color of the ore
+  * mining_time -> Changes the mining tme from the default of 1 (optional)
+  * result -> Makes the mining result different from the ore name (optional)
+  * fluid -> Specifies the fluid needed for mining (optional)
+  * fluid_amount -> Specifies the amount of fluid needed for mining (optional, default is 10)
+
+* Valid values for autoplace_properties:
+  * base_density -> The base amount of the ore patch sizes before multipliers
+  * in_starting_area -> Does the resource spawn in the starting area? (optional, false by default)
+  * regular_multiplier -> The multiplier for ore amount outside of the starting area (optional, default is 1)
+  * starting_area_multiplier -> The multiplier for ore amount inside the starting area (optional, default is 1.1)
